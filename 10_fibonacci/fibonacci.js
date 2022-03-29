2c774d3406f332339830b2044d8758b76901b2e4
Eric Kong <erickongdata@gmail.com>
2022-03-29 18:42:32 +0100

    10-11 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  const seq = [1, 1];
+  if (num < 0) return "OOPS";
+  if (num < 3) return 1;
+  for (let i = 0; i < num - 2; i++) {
+    const nextNum = seq[0] + seq[1];
+    seq.unshift(nextNum);
+  }
+  return seq[0];
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  // return books.reduce((obj, book) => {
+  //   obj.push(book.title);
+  //   return obj;
+  // }, []);
+  return books.map(book => book.title);
 };
+
+// const books = [
+//   {
+//     title: "Book",
+//     author: "Name",
+//   },
+//   {
+//     title: "Book2",
+//     author: "Name2",
+//   },
+// ];
+
+// getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
